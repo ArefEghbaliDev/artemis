c0cd7207852ec239696757c8f9315a1567671ae3
Aref <arefeghbalidev@outlook.com>
2022-05-24 21:43:45 +0430

    Add popping font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+
+import 'styles/tailwind.scss';
+import 'styles/main.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
