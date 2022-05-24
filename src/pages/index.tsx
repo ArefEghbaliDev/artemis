85f8062d8e760d58d1d9320ac5cd656a379bfba3
Aref <arefeghbalidev@outlook.com>
2022-05-24 21:56:31 +0430

    Add color palettes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ const Home: NextPage = () => {
     return (
         <div>
             <h1>Hello World</h1>
+            <button type="button" className="px-3 py-2 border-none bg-primary-500 flex rounded">
+                Click Me
+            </button>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                dark: {
+                    500: '#100F1A',
+                    400: '#181627',
+                    300: '#201D34',
+                },
+                primary: {
+                    600: '#2F0AFF',
+                    500: '#401FFF',
+                    400: '#5233FF',
+                },
+            },
+        },
+        colors: {
+            white: {
+                500: '#FFFDF7',
+            },
+        },
     },
     plugins: [],
 };
