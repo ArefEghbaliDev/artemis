b16c7e46f72410fb343f6c708fe95dd8bf42a71f
Aref <arefeghbalidev@outlook.com>
2022-06-04 18:05:16 +0430

    Add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,26 @@
+import React from 'react';
 import type { AppProps } from 'next/app';
 
 import 'assets/styles/tailwind.scss';
 import 'assets/styles/main.scss';
 import NotificationProvider from 'services/context/NotificationContext/NotificationProvider';
 
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from 'services/react-query/queryClient';
+import { Provider } from 'react-redux';
+import { store } from 'services/redux/store';
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <NotificationProvider>
-            <Component {...pageProps} />
-        </NotificationProvider>
+        <React.StrictMode>
+            <Provider store={store}>
+                <QueryClientProvider client={queryClient}>
+                    <NotificationProvider>
+                        <Component {...pageProps} />
+                    </NotificationProvider>
+                </QueryClientProvider>
+            </Provider>
+        </React.StrictMode>
     );
 }
 
